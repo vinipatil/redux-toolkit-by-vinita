6c4746384aad54b54db8cfeae84ac8ea470c2c03
Vinita Patil <vinipatil2325@gmail.com>
2024-04-29 10:20:28 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -9,21 +9,21 @@ const App = () => {
   const dispatch = useDispatch();
 
   const groceryItems = [
-    { id: 1, name: 'Apple', price: 1.5, image: 'https://m.media-amazon.com/images/I/418r5a8VZ9L._SX300_SY300_QL70_FMwebp_.jpg' },
-    { id: 2, name: 'Banana', price: 0.5, image: 'https://m.media-amazon.com/images/I/313UDz7oAhL._SX342_SY445_.jpg' },
-    { id: 3, name: 'Orange', price: 1.0, image: 'https://m.media-amazon.com/images/I/41tRTl-dSjL.jpg' },
-    { id: 4, name: 'Grapes', price: 2.0, image: 'https://m.media-amazon.com/images/I/518Ce251n1L._SX300_SY300_QL70_FMwebp_.jpg' },
-    { id: 5, name: 'Strawberry', price: 3.0, image: 'https://m.media-amazon.com/images/I/519zzF0Rs8L._SY300_SX300_QL70_FMwebp_.jpg' },
-    { id: 6, name: 'Watermelon', price: 5.0, image: 'https://m.media-amazon.com/images/I/41uGoDhVWVL._SX300_SY300_QL70_FMwebp_.jpg' },
-    { id: 7, name: 'Pineapple', price: 2.5, image: 'https://m.media-amazon.com/images/I/41XubsgLveL._SY300_SX300_QL70_FMwebp_.jpg' },
-    { id: 8, name: 'Kiwi', price: 1.8, image: 'https://m.media-amazon.com/images/I/41hBhaVhNtL._SY300_SX300_QL70_FMwebp_.jpg' },
-    { id: 9, name: 'Mango', price: 2.5, image: 'https://m.media-amazon.com/images/I/51FRr8EcA8L._SX679_.jpg' },
-    { id: 10, name: 'Pear', price: 1.2, image: 'https://m.media-amazon.com/images/I/41YYLKLwOFL._SX679_.jpg' },
-    { id: 11, name: 'Lemon', price: 0.8, image: 'https://m.media-amazon.com/images/I/31Mc9SQs9AL._SY300_SX300_QL70_FMwebp_.jpg' },
-    { id: 12, name: 'Cherry', price: 2.0, image: 'https://m.media-amazon.com/images/I/51VOp8IduZL._SX300_SY300_QL70_FMwebp_.jpg' },
-    { id: 13, name: 'Peach', price: 1.7, image: 'https://m.media-amazon.com/images/I/41w5AsmdIcL.jpg' },
-    { id: 14, name: 'Pomegranate', price: 1.3, image: 'https://m.media-amazon.com/images/I/4175wQ0LTgL._SY300_SX300_QL70_FMwebp_.jpg' },
-    { id: 15, name: 'Guava', price: 1.6, image: 'https://m.media-amazon.com/images/I/41S4xXyjIzL._SX300_SY300_QL70_FMwebp_.jpg' },
+    { id: 1, name: 'Apple', price: 30, image: 'https://m.media-amazon.com/images/I/418r5a8VZ9L._SX300_SY300_QL70_FMwebp_.jpg' },
+    { id: 2, name: 'Banana', price: 30, image: 'https://m.media-amazon.com/images/I/313UDz7oAhL._SX342_SY445_.jpg' },
+    { id: 3, name: 'Orange', price: 50, image: 'https://m.media-amazon.com/images/I/41tRTl-dSjL.jpg' },
+    { id: 4, name: 'Grapes', price: 50, image: 'https://m.media-amazon.com/images/I/518Ce251n1L._SX300_SY300_QL70_FMwebp_.jpg' },
+    { id: 5, name: 'Strawberry', price: 40, image: 'https://m.media-amazon.com/images/I/519zzF0Rs8L._SY300_SX300_QL70_FMwebp_.jpg' },
+    { id: 6, name: 'Watermelon', price: 50, image: 'https://m.media-amazon.com/images/I/41uGoDhVWVL._SX300_SY300_QL70_FMwebp_.jpg' },
+    { id: 7, name: 'Pineapple', price: 30, image: 'https://m.media-amazon.com/images/I/41XubsgLveL._SY300_SX300_QL70_FMwebp_.jpg' },
+    { id: 8, name: 'Kiwi', price: 30, image: 'https://m.media-amazon.com/images/I/41hBhaVhNtL._SY300_SX300_QL70_FMwebp_.jpg' },
+    { id: 9, name: 'Mango', price: 50, image: 'https://m.media-amazon.com/images/I/51FRr8EcA8L._SX679_.jpg' },
+    { id: 10, name: 'Pear', price: 30, image: 'https://m.media-amazon.com/images/I/41YYLKLwOFL._SX679_.jpg' },
+    { id: 11, name: 'Lemon', price: 60, image: 'https://m.media-amazon.com/images/I/31Mc9SQs9AL._SY300_SX300_QL70_FMwebp_.jpg' },
+    { id: 12, name: 'Cherry', price: 50, image: 'https://m.media-amazon.com/images/I/51VOp8IduZL._SX300_SY300_QL70_FMwebp_.jpg' },
+    { id: 13, name: 'Peach', price: 40, image: 'https://m.media-amazon.com/images/I/41w5AsmdIcL.jpg' },
+    { id: 14, name: 'Pomegranate', price: 50, image: 'https://m.media-amazon.com/images/I/4175wQ0LTgL._SY300_SX300_QL70_FMwebp_.jpg' },
+    { id: 15, name: 'Guava', price: 50, image: 'https://m.media-amazon.com/images/I/41S4xXyjIzL._SX300_SY300_QL70_FMwebp_.jpg' },
   ];
 
   // Your cart items from the Redux store
